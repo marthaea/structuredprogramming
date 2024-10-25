9a877b30331fa20c68469d3d0a158ac95d7a7a01
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-25 22:20:19 +0300

    Add files via upload

---
+++ personalpractice.py
@@ -0,0 +1,42 @@
+#not equal
+a = 5
+b = 3
+result = a != b
+print(result)
+#greater than
+a = 7
+b = 3
+result = a > b
+print(result)
+
+#string operations
+a = "Hello"
+b = "world"
+result = a +''+ b
+print(result)
+#it can also be a + b
+
+#length
+a = "Python"
+result = len(a)
+print(result)
+
+#uppercase
+a = "python"
+result = a.upper()
+print(result)
+
+y = "martha"
+result = y.upper()
+print(result)
+
+#lowercase
+g = "PYTHON"
+result = g.lower()
+print(result)
+
+#listoperations#
+#indexing
+my_list = [1,2,3]
+result = my_list[1]
+print(my_list)
